676e055f22340d40a0c15391feca2124d0374ee4
Adam Campbell <campbell.e.adam@gmail.com>
2021-03-17 16:07:05 -0600

    Updated routes to use gorup

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('users', UserController::class);
+Route::middleware(['auth'])->group(function (){
+    
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('users', UserController::class);
+});
