552d12c2d0a5d287d480835e7f259a28fd56971f
Adam Campbell <campbell.e.adam@gmail.com>
2021-04-14 17:22:02 -0600

    user login json

--- routes/api.php
+++ routes/api.php
@@ -56,7 +56,8 @@ Route::post('v1/user/login', function (Request $request) {
         ]);
     }
 
-    return $user->createToken($request->device_name)->plainTextToken;
+    return response()->json([$user->createToken($request->device_name)->plainTextToken]);
+    //return $user->createToken($request->device_name)->plainTextToken;
     
 });
 
