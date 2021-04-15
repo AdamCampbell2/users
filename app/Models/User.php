348c2142cfe3aca13395fab6827f8a8c5abe2dc9
Adam Campbell <campbell.e.adam@gmail.com>
2021-04-15 13:02:03 -0600

    get user laps

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\ClimbAMile;
 
 class User extends Authenticatable
 {
@@ -47,5 +48,10 @@ class User extends Authenticatable
     public function users()
     {
         return $this->hasMany(User::class, 'created_by');
+    }
+
+    public function laps()
+    {
+        return $this->hasMany(ClimbAMile::class);
     }
 }
