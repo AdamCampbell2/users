ab1de6a9784d11819558e5b6d36c9c08ce81b1eb
Adam Campbell <campbell.e.adam@gmail.com>
2021-04-14 19:24:48 -0600

    move route

--- routes/api.php
+++ routes/api.php
@@ -29,12 +29,13 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::prefix('v1')->group(function(){
         Route::apiResource('users', UserController::class);
 
+        });
+
         Route::apiResource('laps', function(Request $request){
             $user = DB::table('personal_access_tokens')->where('tokenable_id','=', '21');
             $user_laps = DB::table('users')->where('id','=', $user);
             return $user_laps;
 
-        });
        // Route::apiResource('laps', ClimbAMileController::class);
 
     });
